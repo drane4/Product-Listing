d41dae19c36c4dfb28431f480db2d91ffebeb0d6
drane4 <mjgardose@gmail.com>
2018-01-22 15:06:41 +0800

    3rd commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true, 
   firebase:{
-    
+    apiKey: "AIzaSyDBO28FTYtleIWU0V7ejsivS1R7GgBftO8",
+    authDomain: "firestore-crud-of-users-2c81a.firebaseapp.com",
+    databaseURL: "https://firestore-crud-of-users-2c81a.firebaseio.com",
+    projectId: "firestore-crud-of-users-2c81a",
+    storageBucket: "firestore-crud-of-users-2c81a.appspot.com",
+    messagingSenderId: "960666650052"
   }
 };
